cf0f2c5d2efbe72339a5a5f0bfd5b0e3ed860fe0
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-02-06 12:08:15 +0000

    Started adding branch and bound algorithm in python

--- example_miqp.py
+++ example_miqp.py
@@ -1,26 +1,28 @@
 """
 Solve MIQP using mathprogbasepy
 """
+from __future__ import division
 import scipy as sp
 import scipy.sparse as spspa
 import numpy as np
 import mathprogbasepy as mpbpy
 
 
-# Random Example
-n = 30
-m = 50
-# Generate random Matrices
-Pt = sp.randn(n, n)
-P = spspa.csc_matrix(np.dot(Pt.T, Pt))
-q = sp.randn(n)
-A = spspa.csc_matrix(sp.randn(m, n))
-u = 3 + sp.randn(m)
-# l = u
-l = -3 + sp.randn(m)
-i_idx = np.array([0, 3, 7])  # index of integer variables
+if __name__ == "__main__":
+    # Random Example
+    n = 30
+    m = 50
+    # Generate random Matrices
+    Pt = sp.randn(n, n)
+    P = spspa.csc_matrix(np.dot(Pt.T, Pt))
+    q = sp.randn(n)
+    A = spspa.csc_matrix(sp.randn(m, n))
+    u = 3 + sp.randn(m)
+    # l = u
+    l = -3 + sp.randn(m)
+    i_idx = np.array([0, 3, 7])  # index of integer variables
 
-# Create MIQP problem
-prob = mpbpy.QuadprogProblem(P, q, A, l, u, i_idx)
-resGUROBI = prob.solve(solver=mpbpy.GUROBI)
-resCPLEX = prob.solve(solver=mpbpy.CPLEX)
+    # Create MIQP problem
+    prob = mpbpy.QuadprogProblem(P, q, A, l, u, i_idx)
+    resGUROBI = prob.solve(solver=mpbpy.GUROBI)
+    resCPLEX = prob.solve(solver=mpbpy.CPLEX)
--- miosqp.py
+++ miosqp.py
@@ -2,4 +2,71 @@
 Solve Mixed-Integer QPs using Branch and Bound and OSQP Solver
 """
 
+from __future__ import print
 import osqp  # Import OSQP Solver
+import numpy as np
+
+
+def choose_leaf(leaves, u_glob, branch_rule):
+    """
+    Choose next leaf to branch depending on rule
+    """
+    if branch_rule == 0:
+        leaf = argminl(leaves, u_glob)
+    else:
+        rause ValueError('Branching rule not recognized')
+
+
+def branch_leaf(leaf, leaves):
+    """
+    Expand leaf within leaves list in branch and bound tree
+    """
+    left = l.add_left()
+    right = l.add_right()
+    leaves.remove(l)
+    leaves += [left, right]
+    return left, right
+
+
+def miosqp_solve(P, q, A, l, u, i_idx):
+    """
+    Solve MIQP problem using MIOSQP solver
+    """
+
+    # Branch and bound parameters
+    eps_bb = 1e-03          # tolerance for difference between upper and lower bound
+    eps_int_feas = 1e-03    # tolerance for integer feasibility
+    branch_rule = 0         # branching rule [0] lowest lower bound
+
+    # Extend problem with new constraints
+    # Extend matrix A and bounds l and u
+
+    # Initialize branch and bound iterations
+    iter = 0
+    u_glob = root.solve()
+    l_glob = -np.inf
+    leaves = [top]
+    # masses = []
+    # massesind = []
+    
+    # Loop tree until the gap has disappeared
+    while u_glob - l_glob > eps_bb:
+
+        # Choose leaf to branch depending on branch_rule
+        l = choose_leaf(leaves, u_glob, branch_rule)
+
+        # Expand best leaf
+        left, right = branch_leaf(l, leaves)
+
+        # Update lower and upper bound
+        l_glob = min([x.lower for x in leaves])
+        u_glob = min(u_glob, left.upper, right.upper)
+
+        # Update iteration number
+        iter += 1
+
+        # Print progress
+        print("iter %.3d   lower bound: %.5f, upper bound %.5f" % (iter,
+            l_glob, u_glob))
+
+
