af8bd53224412311070016ba8697d68944007af1
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-02-09 23:28:27 +0000

    Made progress. Now need to define function to deal separately with root node and then we are ready

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 
 ## TODO
 
--   [ ] Add functions: add_left and add_right for each node
-    -   [ ] N.B. Update node l_i and u_i by restricting newly introduced ones in branching
--   [ ] Store solution when upper bound improves (only x, dual does not matter)
--   [ ] Add function to deal separately with root node
--   [ ] Add get_bounds function
--   [ ] Add proper warm starting (from parent node)
+-   [x] Add functions: add_left and add_right for each node
+    -   [x] N.B. Update node l_i and u_i by restricting newly introduced ones in branching
+-   [x] Store solution when upper bound improves (only x, dual does not matter)
+-   [ ] Add function to deal separately with root node (when branching)
+-   [x] Complete get_bounds function
+-   [x] Add proper warm starting (from parent node, when branching)
+-   [x] Add only workspace passed to the nodes!
--- miosqp.py
+++ miosqp.py
@@ -110,31 +110,25 @@ class Node:
         vector of lower bounds in relaxed QP problem
     u: numpy array
         vector of upper bounds in relaxed QP problem
-    solver: instance of QP solver class
-        reference to QP solver  object
-    settings: Settings
-        MIQP solver settings
-    data: Data
-        MIQP problem data
     x: numpy array
         node's integer (possibly not feasible) MIQP solution
     x_relaxed: numpy array
-        node's relaxed solution
-    x0: numpy array
-        warm_start primal variable
-    y0: numpy array
-        warm_start dual variable
+        node's relaxed solution. At the beginning it is the warm-starting value x0
+    y_relaxed: numpy array
+        node's relaxed solution. At the beginning it is the warm-starting value y0
     status: int
         node's status
     nextvar: int
-        index of next variable to split upon
+        index of next variable to split upon (position within i_idx vector)
     left: Node
         left child
     right: Node
         right child
+    work: Workspace
+        MIQP solver workspace
     """
 
-    def __init__(self, l, u, solver, settings, data,
+    def __init__(self, l, u, work,
                  x0=None, y0=None):
         """
         Initialize node class
@@ -148,31 +142,19 @@ class Node:
         self.l = l
         self.u = u
 
-        # Assign settings member
-        self.settings = settings
-
-        # Assign data member
-        self.data = data
-
-        # Link solver
-        self.solver = solver
-
-        # Update l and u in the solver instance
-        self.solver.update(l=self.l, u=self.u)
+        # Assign solver Workspace
+        self.work = work
 
         # Integer MIQP solution
         self.x = None
 
-        # Relaxed MIQP solution
-        self.x_relaxed = None
-
-        # Warm-start variables
+        # Warm-start variables which are also the relaxed solutions
         if x0 is None:
-            x0 = np.zeros(self.data.n)
+            x0 = np.zeros(self.work.data.n)
         if y0 is None:
-            y0 = np.zeros(self.data.m)
-        self.x0 = x0
-        self.y0 = y0
+            y0 = np.zeros(self.work.data.m)
+        self.x_relaxed = x0
+        self.y_relaxed = y0
 
         # Set node status
         self.status = MI_NODE_PENDING
@@ -192,9 +174,9 @@ class Node:
         if self.status != MI_NODE_FATHOMED:
             nodes_list += [self]
         if self.left is not None and self.left.status != MI_NODE_FATHOMED:
-            nodes_list += self.left.nodes(self)
+            nodes_list += self.left.nodes()
         if self.right is not None and self.right.status != MI_NODE_FATHOMED:
-            nodes_list += self.right.nodes(self)
+            nodes_list += self.right.nodes()
 
         return nodes_list
 
@@ -202,21 +184,67 @@ class Node:
         """
         Add node to the left-hand side of the tree and solve it
         """
+        # Get next variable constraint idx
+        constr_idx = self.work.data.m + self.nextvar
+
+        # Get next variable idx
+        nextvar_idx = self.work.data.i_idx[self.nextvar]
+
+        # Get updated QP interval bounds
+        l_left = self.l
+        u_left = self.u
+
+        # Add constraint if it make the feasible region smaller (min())
+        # x <= floor(x_relaxed)
+        u_left[constr_idx] = min(u_left[constr_idx],
+                                 np.floor(self.x_relaxed[nextvar_idx]))
+
+        if self.left is None:
+            # Create node with new bounds
+            self.left = Node(l_left, u_left, self.work,
+                             self.x_relaxed, self.y_relaxed)
+            # Get bounds from that node
+            self.left.get_bounds()
+
+        return self.left
+
 
     def add_right(self):
         """
-        Add node to the left-hand side of the tree and solve it
+        Add node to the right-hand side of the tree and solve it
         """
+        # Get next variable constraint idx
+        constr_idx = self.work.data.m + self.nextvar
+
+        # Get next variable idx
+        nextvar_idx = self.work.data.i_idx[self.nextvar]
+
+        # Get updated QP interval bounds
+        l_right = self.l
+        u_right = self.u
+
+        # Add constraint if it make the feasible region smaller (max())
+        # ceil(x_relaxed) <= x
+        l_right[constr_idx] = max(l_right[constr_idx],
+                                 np.ceil(self.x_relaxed[nextvar_idx]))
+
+        if self.right is None:
+            # Create node with new bounds
+            self.right = Node(l_right, u_right, self.work,
+                              self.x_relaxed, self.y_relaxed)
+            # Get bounds from that node
+            self.right.get_bounds()
+
+        return self.right
 
     def is_within_bounds(self, x):
         """
         Check if current solution is within bounds
         """
 
-        # Check if it satisfies original problem bounds
-        z = self.data.A[:self.data.m,:].dot(x)
-        if any(z < self.data.l[:self.data.m]) | \
-            any(z > self.data.u[:self.data.m]):
+        # Check if it satisfies current l and u bounds
+        z = self.work.data.A.dot(x)
+        if any(z < self.l) | any(z > self.u):
             return False
 
         # If we got here, it is integer feasible
@@ -229,14 +257,8 @@ class Node:
         """
 
         # Check if integer variables are feasible up to tolerance
-        x_int = x[self.data.i_idx]
-        if any(abs(x_int - np.round(x_int))) > self.settings.eps_int_feas:
-            return False
-
-        # Check if it satisfies original problem bounds
-        z = self.data.A[:self.data.m,:].dot(x)
-        if any(z < self.data.l[:self.data.m]) | \
-            any(z > self.data.u[:self.data.m]):
+        x_int = x[self.work.data.i_idx]
+        if any(abs(x_int - np.round(x_int))) > self.work.settings.eps_int_feas:
             return False
 
         # If we got here, it is integer feasible
@@ -247,7 +269,7 @@ class Node:
         Round obtained solution to integer feasibility
         """
         x_int = x
-        x_int[self.data.i_idx] = np.round(x[self.data.i_idx])
+        x_int[self.work.data.i_idx] = np.round(x[self.work.data.i_idx])
         return x_int
 
     def pick_nextvar(self, x):
@@ -255,31 +277,57 @@ class Node:
         Pick next variable to branch upon
         """
 
+        # Part of solution vector that is supposed to be integer
+        x_int = x[self.work.data.i_idx]
 
+        if self.work.settings['branching_rule'] == 0:
 
+            # Get vector of fractional parts
+            fract_part = abs(x_int - np.round(x_int))
+
+            # Get index of max fractional part
+            next_idx = np.argmax(fract_part)
+
+            # Get index of next variable as position in the i_idx vector
+            self.nextvar = next_idx
+        else:
+            raise ValueError('No variable selection rule recognized!')
 
     def get_bounds(self):
         """
         Find upper and lower bounds for the relaxed problem corresponding to this node
         """
+        # Update l and u in the solver instance
+        self.work.solver.update(l=self.l, u=self.u)
 
         # Warm start solver with currently stored solution
-        self.solver.warm_start(x=self.x0, y=self.y0)
+        self.work.solver.warm_start(x=self.x_relaxed, y=self.y_relaxed)
 
         # Solve current problem
-        results = self.solver.solve()
+        results = self.work.solver.solve()
+
+        # Check if maximum number of iterations reached
+        if (results.info.status_val == \
+            self.work.solver.constants('OSQP_MAX_ITER_REACHED')):
+            print("ERROR: Max Iter Reached!")
+            from ipdb import set_trace; set_trace()
 
         # Check if infeasible or unbounded -> Node becomes fathomed
-        if (results.info.status_val == self.solver.constant('OSQP_INFEASIBLE')) | \
-            results.info.status_val == self.solver.constant('OSQP_UNBOUNDED'):
+        if (results.info.status_val == \
+            self.work.solver.constant('OSQP_INFEASIBLE')) | \
+            results.info.status_val == \
+            self.work.solver.constant('OSQP_UNBOUNDED'):
             self.status = MI_NODE_FATHOMED
             return
         else:
             self.status = MI_NODE_SOLVED
             self.x_relaxed = results.x
+            self.y_relaxed = results.y
 
-        # Check if integer feasible solution -> update bounds, status and variable x
-        if (self.is_int_feas(self.x_relaxed)):
+        # Check if integer feasible solution withing l and u bounds
+        if (self.is_int_feas(self.x_relaxed) and \
+            self.is_within_bounds(self.x_relaxed)):
+            #   -> update bounds, status and variable x
             self.lower = results.info.obj_val
             self.upper = results.info.obj_val
             self.status = MI_NODE_FATHOMED
@@ -295,7 +343,7 @@ class Node:
         #   - check feasibility
         if self.is_within_bounds(self.x):
             #   - if rounded is feasible --> compute new upper bound
-            self.upper = self.data.compute_obj_val(self.x)
+            self.upper = self.work.data.compute_obj_val(self.x)
         else:
             self.x = None
 
@@ -350,8 +398,7 @@ class Workspace(object):
 
 
         # Define root node
-        self.root = Node(self.data.l, self.data.u, self.solver,
-                         self.settings, self.data)
+        self.root = Node(self.data.l, self.data.u, self)
         self.leaves = [self.root]  # At the initialization there is only the root node
 
         # Define other internal variables
@@ -412,7 +459,12 @@ class Workspace(object):
 
         # Update upper bound
         self.upper_glob = min(self.upper_glob, left.upper, right.upper)
+
         # if uppwer bound improved -> Store node solution x
+        if abs(self.upper_glob - left.upper) < 1e-08:
+            self.x = left.x # Update solution
+        elif abs(self.upper_glob - right.upper) < 1e-08:
+            self.x = right.x # Update solution
 
 
     def bound(self):
@@ -479,9 +531,8 @@ def miosqp_solve(P, q, A, l, u, i_idx):
                 'max_iter_bb': 1000,           # maximum number of iterations
                 'tree_explor_rule': 0,         # tree exploration rule
                                                #   [0] lowest lower bound
-                'branching_rule': 0,           # branching rule
+                'branching_rule': 0}           # branching rule
                                                #   [0] max fractional part
-                'variable_selection_rule': 0}  # select next variable to split upon
 
 
     # Create data class instance
