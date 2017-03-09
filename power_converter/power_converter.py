b8b5a45b2292c456637202fea728f5e0f16689cd
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-03-09 12:27:02 +0000

    Completed model setup. Now writing simulation loop

---
+++ power_converter/power_converter.py
@@ -0,0 +1,427 @@
+import numpy as np
+import numpy.linalg as nla
+import scipy.linalg as sla
+import math
+
+# Internal functions and objects
+from tail_cost import TailCost
+from quadratic_program import MIQP
+
+class DynamicalSystem(object):
+    """
+    Power converter dynamical system
+    """
+    def __init__(self, params, fsw_des, delta):
+        """
+        Generate extended ADP model
+
+
+        Args:
+        params (dict): model parameters
+        fsw_des (double): desired switching frequency
+        delta (double): relative weighting cost function
+
+
+        Returns:
+        model (dict): extended adp model
+        """
+
+        # Get parameters
+        taus = params.taus
+        taur = params.taur
+        D = params.D
+        omegar = params.omegar
+        Vdc = params.Vdc
+        Xm = params.Xm
+        Xr = params.Xr
+        P = params.P
+        Tspu = params.Tspu
+        k1 = params.k1
+        k2 = params.k2
+        Ts = params.Ts
+
+
+        '''
+        Generate individual system matrices
+        '''
+        # Physical system matrices
+        F = np.array([[-1./taus, 0., Xm/(taur * D), omegar*Xm/D],
+                      [0., -1./taus, -omegar*Xm/D, Xm/(taur * D)],
+                      [Xm/taur, 0., -1./taur, -omegar],
+                      [0., Xm/taur, omegar, -1./taur]])
+
+        G = Xr / D * Vdc / 2. * \
+            np.array([[1., 0], [0., 1], [0., 0.], [0., 0.]]).dot(P)
+
+
+        # Discretize physical system
+        A_phys = sla.expm(F * Tspu)
+        B_phys = -(nla.inv(F).dot(np.eye(A_phys.shape[0]) - A_phys).dot(G))
+
+
+        # Concatenate oscillating states
+        A_osc = np.array([[np.cos(Tspu), -np.sin(Tspu)],
+                         [np.sin(Tspu), np.cos(Tspu)]])
+        B_osc = np.zeros((2, 3))
+
+        # Concatenate previous input as a state
+        A_prev = np.zeros((3, 3))
+        B_prev = np.eye(3)
+
+
+        # Concatenate filter states
+        a1 = 1. - 1./k1
+        a2 = 1. - 1./k2
+
+        A_sw = np.array([[a1, 0.],
+                         [(1. - a1), a2]])
+        # NB 1: Please note the 1 / 12 division to average over all the physical switches
+        # NB 2: Please note the 1/fsw_des division to normalize switching frequency
+        B_sw = 1./ fsw_des * 1./12. * (1 - a1) / Ts * \
+            np.array([[1., 1., 1.], [0., 0., 0.]])
+
+
+
+        # Concatenate switching frequency state
+        A_fsw = np.array([[1.]])
+
+        '''
+        Generate complete system
+        '''
+        A = sla.block_diag(A_phys, A_osc, A_prev, A_sw, A_fsw)
+        B = np.bmat([[B_phys, np.zeros((4, 3))],
+                     [B_osc, np.zeros((2, 3))],
+                     [B_prev, np.zeros((3, 3))],
+                     [np.zeros((2, 3)), B_sw],
+                     [np.zeros((1, 6))]])
+        C = np.array([[1., 0., 0., 0., -1., 0., 0., 0., 0., 0., 0., 0.],
+                      [0., 1., 0., 0., 0., -1., 0., 0., 0., 0., 0., 0.,],
+                      [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., -delta, delta]])
+
+
+        '''
+        Extract previous input from data
+        '''
+        W = np.hstack((np.zeros((3, 6)), np.eye(3), np.zeros((3, 3))))
+
+        '''
+        Matrix to extract original input and auxiliary one from state
+        '''
+        G = np.hstack((np.eye(3), np.zeros((3, 3))))
+        T = np.hstack((np.zeros((3, 3)), np.eye(3)))
+
+
+        '''
+        Generate matrix of possible input combinations
+        '''
+        M = np.zeros((3, 27))
+        M[0, :] = np.hstack((-np.ones((1, 9)), np.zeros((1, 9)), np.ones((1, 9))))
+        M[1, :] = np.tile(np.hstack((-np.ones((1, 3)), np.zeros((1, 3)),
+                                     np.ones((1, 3)))), (1, 3))
+        M[2, :] = np.tile(np.array([-1, 0, 1]), (1, 9))
+
+
+        '''
+        Define system attributes
+        '''
+        self.A = A
+        self.B = B
+        self.C = C
+        self.W = W
+        self.G = G
+        self.T = T
+        self.M = M
+        self.fsw_des = fsw_des
+        self.delta = delta
+
+
+
+
+
+class InitialConditions(object):
+    """
+    Power converter initial conditions
+
+    Attributes:
+        x0 (array): initial state for simulations
+        cur_step_torque (array): currents when there is a torque step
+    """
+
+    def __init__(self, params):
+        """
+        Setup initial conditions
+        """
+        torque = params.torque
+
+        # Compute steady state values with T
+        x0 = self.get_initial_states(params, torque, 1)
+        x_null_torque = self.get_initial_states( params, 0., 1. )
+
+        # Previous input
+        uprev = np.array([0., 0., 0.])
+
+        r'''
+        Current at the torque step
+
+        Te = 0 =>
+                $i_{s\beta} = i_{s\alpha} \frac{\phi_{r\beta}}{\phi_{r\alpha}}$
+
+        '''
+        cur_step_torque = np.array([-x_null_torque[0],
+                                    -x_null_torque[0] * x0[3] / x0[2]])
+
+        self.x0 = np.concatenate((x0, x0[:2], uprev, np.array([1., 1., 1.])))
+        self.cur_step_torque = cur_step_torque
+
+
+
+    def get_initial_states(self, params, T, psiS_mag):
+        """
+        Given the torque and stator flux magnitude, compute
+        the stator and rotor flux vectors in alpha/beta and the slip
+        frequency. The stator flux vector is aligned with the
+        alpha-axis.
+
+
+        Args:
+            params (dict): parameters
+            T (double): torque reference [pu]
+            psiS_mag (double): reference of the stator flux magnitude [pu]
+
+
+        Returns:
+            x0 (array): initial state [i_s, phi_r]
+        """
+
+
+        # Get parameters
+        Rs = params.Rs
+        Rr = params.Rr
+        Xss = params.Xs
+        Xrr = params.Xr
+        Xm = params.Xm
+        D = params.D
+        kT = params.kT  # Torque constant to correct [pu]
+
+
+        # Stator flux components
+        psiSa = psiS_mag
+        psiSb = 0
+
+        # PsiR alpha and beta components
+        psiRb = -T/psiS_mag * D / Xm / kT
+        dis = np.sqrt((Xm**2)*(psiSa**2) - 4. * (Xss**2) * (psiRb**2))
+        psiRa1 = (Xm*psiSa + dis)/(2. * Xss)
+        psiRa2 = (Xm*psiSa - dis)/(2*Xss)
+
+        psiRa = psiRa1  # make sure that this is always the correct choice!
+
+        # Slip frequency
+        ws = -Rr * Xss / D * psiRb / psiRa
+
+        # Build stator and rotor flux vectors
+        PsiS = np.array([psiSa,  psiSb])
+        PsiR = np.array([psiRa, psiRb])
+
+        # Initialize the transformation matrix M
+        M = 1./ D * np.array([[Xrr, 0, -Xm, 0.],
+                              [0, Xrr, 0, -Xm]])
+
+
+        # Stator currents in alpha/beta
+        Is = M.dot(np.append(PsiS, PsiR))
+
+        # Initial state
+        x0 = np.append(Is, PsiR)
+
+        return x0
+
+
+class Params(object):
+    """
+    Power converter model parameters
+    """
+
+    def __init__(self, Ts, freq, k1, k2, torque):
+        """
+        Compute model parameters given
+
+        Args:
+            Ts (double): Sampling time
+            freq (double): Frequency
+            k1 (double): filter constant
+            k2 (double): filter constant
+            torque (double): motor torque
+        """
+
+        # Resistance
+        Rs = 0.0108  # Stator
+        Rr = 0.0091  # Rotor
+
+        # Inductances
+        Xls = 0.1493  # Stator
+        Xlr = 0.1104  # Rotor
+        Xm = 2.3489   # Mutual
+
+        # Rotor speed
+        omegar = 0.9911 # Nominal speed
+
+        # Voltages
+        Vdc = 1.930
+
+        # Torque constant to fix [pu] units
+        kT = 1.2361
+
+        '''
+        Define base model units
+        '''
+        omegab = 2 * math.pi * freq
+        Tspu = Ts * omegab
+        Nstpp = 1./freq/Ts  # Number of steps per period
+
+
+        '''
+        Compute intermediate parameters
+        '''
+
+        # Inductances
+        Xs = Xls + Xm
+        Xr = Xlr + Xm
+        D = 0.6266   # D = Xs * Xr - Xm ^ 2
+
+        # Time constants
+        taus = (Xr * D) / (Rs * (Xr**2) + Rr * (Xm ** 2))
+        taur = Xr / Rr
+
+
+        '''
+        Define Park transformation and its inverse
+        '''
+        P = 2./3. * np.array([[1.0, -1./2., -1./2.],
+                              [0.0, np.sqrt(3.)/2., -np.sqrt(3.)/2.]])
+
+        invP = np.array([ [1., 0.],
+                          [-1./2., np.sqrt(3.)/2.],
+                          [-1./2., -np.sqrt(3.)/2.]])
+
+
+        '''
+        Store model parameters
+        '''
+        self.Rs = Rs
+        self.Rr = Rr
+        self.Xm = Xm
+        self.Xs = Xs
+        self.Xr = Xr
+        self.D = D
+        self.taus = taus
+        self.taur = taur
+        self.omegar = omegar
+        self.omegab = omegab
+        self.Vdc = Vdc
+        self.kT = kT
+        self.Tspu = Tspu
+        self.Nstpp = Nstpp
+        self.P = P
+        self.invP = invP
+        self.freq = freq
+        self.Ts = Ts
+        self.k1 = k1
+        self.k2 = k2
+        self.torque = torque
+
+
+class Time(object):
+    """
+    Power converter time structure
+    """
+
+    def __init__(self, t0, Ts, init_periods, sim_periods, Nstpp):
+        """
+        Setup time object
+        """
+        self.init_periods = init_periods
+        self.sim_periods = sim_periods
+        self.T_final = (init_periods + sim_periods) * Nstpp
+        self.Ts = Ts
+        self.t0 = t0
+        self.t = np.linspace(t0, Ts * self.T_final, self.T_final + 1)
+
+
+
+class Model(object):
+    """
+    Power converter model
+    """
+
+    def __init__(self):
+        self.params = None
+        self.dyn_system = None
+        self.tail_cost = None
+        self.init_conditions = None
+        self.time = None
+        self.qp_matrices = None
+
+    def set_params(self, Ts, freq, k1, k2, torque):
+        self.params = Params(Ts, freq, k1, k2, torque)
+
+    def set_time(self, t0, init_periods, sim_periods):
+        """
+        Set simulation time structure
+        """
+        self.time = Time(t0, self.params.Ts, init_periods, sim_periods,
+                         self.params.Nstpp)
+
+    def set_initial_conditions(self):
+        """
+        Set power converter initial conditions
+        """
+        self.init_conditions = InitialConditions(self.params)
+
+    def gen_dynamical_system(self, fsw_des, delta):
+        """
+        Generate dynamical system given parameters and
+        """
+        self.dyn_system = DynamicalSystem(self.params, fsw_des, delta)
+
+    def gen_tail_cost(self, N_tail, gamma, name=None):
+        '''
+        Compute or load tail cost
+        '''
+        self.tail_cost = TailCost(self.dyn_system, gamma)
+
+        if name is not None:
+            self.tail_cost.load(name)
+        else:
+            self.tail_cost.compute(self.dyn_system, N_tail)
+
+
+    def simulate_cl(self, N, steady_trans):
+        """
+        Perform closed loop simulation
+        """
+
+        # Rename some variables for notation ease
+        nx = self.dyn_system.A.shape[0]
+        nu = self.dyn_system.B.shape[1]
+        ny = self.dyn_system.C.shape[0]
+        T_final = self.time.T_final
+
+
+        # Compute QP matrices
+        self.qp_matrices = MIQP(self.dyn_system, N, self.tail_cost)
+
+
+        # Preallocate vectors of results
+        X = np.zeros((nx, T_final))
+        Y = np.zeros((ny, T_final))
+        Y = np.zeros((nu, T_final))
+        solve_times = np.zeros(T_final)  # Computing times
+        obj_vals = np.zeros(T_final)     # Objective values
+
+
+        # Set initial statte
+        X[:, 0] = self.init_conditions.x0
+
+
+        
---
+++ power_converter/run_example.py
@@ -0,0 +1,75 @@
+"""
+Simulate inverter model with ADP formulation from the paper
+"High-Speed Finite Control Set Model Predictive Control for Power Electronics", B. Stellato, T. Geyer and P. Goulart
+
+"""
+import numpy as np
+
+# Power converter model files
+from power_converter import Model
+from tail_cost import TailCost
+
+
+'''
+Simulation parameters
+'''
+Ts = 25.0e-06            # Sampling time
+freq = 50.               # Switching frequency
+torque = 1.              # Desired torque
+t0 = 0.0                 # Initial time
+init_periods = 4         # Number of integer period to settle before simulation
+sim_periods = 20         # Numer of simulated periods
+flag_steady_trans = 0    # Flag Steady State (0) or Transients (1)
+
+
+'''
+ADP Parameters
+'''
+gamma = 0.95          # Forgetting factor
+N_adp = 1              # Horizon length
+delta = 4             # Switching frequency penalty
+N_tail = 50
+
+# Switching filter parameters
+k1 = 0.8e03
+k2 = 0.8e03
+fsw_des = 300
+
+
+'''
+Setup model
+'''
+model = Model()
+
+# Set model parameters
+model.set_params(Ts, freq, k1, k2)
+
+# Set simulation time
+model.set_time(t0, init_periods, sim_periods)
+
+# Set initial conditions given the desired torque
+model.set_initial_conditions(torque)
+
+'''
+Allocate output statistics
+'''
+THD_adp = []
+fsw_adp = []
+Te_adp = []
+Times_adp = []
+
+
+'''
+Run simulations
+'''
+
+#TODO: Add for loop for different N_adp, fsw_des, delta
+
+# Generate extended adp model
+model.gen_dynamical_system(fsw_des, delta)
+
+# Generate tail cost
+model.gen_tail_cost(N_tail, gamma, name='tailBackupN1Delta4.00.mat')
+
+# Simulate model
+model.simulate_cl(N_adp)
