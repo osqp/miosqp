Example runs with gurobi. It produces wrong results still

example_miqp.py
@@ -57,17 +57,18 @@ if __name__ == "__main__":
 
 
     # Enforce integer variables to be binary => {0, 1}
-    I_int = spa.identity(n).tocsc()
-    I_int = I_int[i_idx, :]
-    l_int = np.empty((n,))
-    l_int.fill(0.)
-    l_int = l_int[i_idx]
-    u_int = np.empty((n,))
-    u_int.fill(1)
-    u_int = u_int[i_idx]
-    A = spa.vstack([A, I_int]).tocsc()      # Extend problem constraints matrix A
-    l = np.append(l, l_int)         # Extend problem constraints
-    u = np.append(u, u_int)         # Extend problem constraints
+    # I_int = spa.identity(n).tocsc()
+    # I_int = I_int[i_idx, :]
+    # l_int = np.empty((n,))
+    # l_int.fill(0.)
+    # l_int = l_int[i_idx]
+    # u_int = np.empty((n,))
+    # u_int.fill(1)
+    # u_int = u_int[i_idx]
+    # A = spa.vstack([A, I_int]).tocsc()      # Extend problem constraints matrix A
+    # l = np.append(l, l_int)         # Extend problem constraints
+    # u = np.append(u, u_int)         # Extend problem constraints
+    A, l, u = miosqp.add_bounds(i_idx, 0., 1., A, l, u)
 
 
 
@@ -90,7 +91,8 @@ if __name__ == "__main__":
                                                 #   [1] two-phase: depth first  until first incumbent and then  best bound
                        'branching_rule': 0,     # branching rule
                                                 #   [0] max fractional part
-                       'verbose': True}
+                       'verbose': True,
+                       'print_interval': 1}
 
     osqp_settings = {'eps_abs': 1e-04,
                      'eps_rel': 1e-04,

power_converter/power_converter.py
@@ -415,14 +415,12 @@ class Model(object):
 
         # Update bounds
         SA_tildex0 = qp.SA_tilde.dot(x0)
-        qp.u[:3 * N] = SA_tildex0
-        qp.l[:3 * N] = -SA_tildex0
+        qp.u[:6 * N] = SA_tildex0
+        qp.l[:6 * N] = -SA_tildex0
 
-        import ipdb; ipdb.set_trace()
-        
         # Solve problem
         prob = mpbpy.QuadprogProblem(qp.P, q, qp.A, qp.l, qp.u, qp.i_idx)
-        res = prob.solve(solver=mpbpy.GUROBI)
+        res = prob.solve(solver=mpbpy.GUROBI, verbose=False)
 
         return res.x, res.obj_val, res.cputime
 
@@ -453,9 +451,9 @@ class Model(object):
 
 
         # Preallocate vectors of results
-        X = np.zeros((nx, T_final))
-        U = np.zeros((ny, T_final))
-        Y = np.zeros((nu, T_final))
+        X = np.zeros((nx, T_final + 1))
+        U = np.zeros((nu, T_final))
+        Y = np.zeros((ny, T_final))
         solve_times = np.zeros(T_final)  # Computing times
         obj_vals = np.zeros(T_final)     # Objective values
 
@@ -463,7 +461,6 @@ class Model(object):
         # Set initial statte
         X[:, 0] = self.init_conditions.x0
 
-        import ipdb; ipdb.set_trace()
         # Run loop
         for i in range(T_final):
 
@@ -471,4 +468,9 @@ class Model(object):
             U[:, i], obj_vals[i], solve_times[i] = self.compute_mpc_input(X[:, i])
 
             # Simulate one step
-            X[:, i+1], Y[:, i+1] = self.simulate_one_step(X[:, i], U[:, i])
+            X[:, i+1], Y[:, i] = self.simulate_one_step(X[:, i], U[:, i])
+
+        import matplotlib.pylab as plt
+        plt.figure()
+        plt.plot(X[1, :])
+        plt.show(block=False)

power_converter/quadratic_program.py
@@ -67,8 +67,9 @@ class MIQP(object):
 
 
         # Quadratic form matrices
-        qp_P = 2. * (B_tilde.T.dot(Hx).dot(B_tilde) + \
-            (gamma**N) * B_tilde_end.T.dot(P0).dot(B_tilde_end))
+        qp_P = spa.csc_matrix(
+            2. * (B_tilde.T.dot(Hx).dot(B_tilde) + \
+            (gamma**N) * B_tilde_end.T.dot(P0).dot(B_tilde_end)))
         qp_q_x = B_tilde.T.dot(Hx.T).dot(A_tilde) + \
             (gamma ** N) * (B_tilde_end).T.dot(P0).dot(A_tilde_end)
         qp_q_u = (gamma ** N) * B_tilde_end.T.dot(q0)
@@ -86,19 +87,31 @@ class MIQP(object):
         '''
 
         # Matrices required for constraint satisfaction S, R, T
-        S = np.kron(np.eye(N), W)
-        S = np.hstack((S, np.zeros((S.shape[0], nx))))
-        R = np.kron(np.eye(N), G - T)
+        S1 = np.hstack(( np.kron(np.eye(N), W), np.zeros((3 * N, nx))))
+        S2 = np.hstack(( np.kron(np.eye(N), -W), np.zeros((3 * N, nx))))
+        S = np.vstack(( S1, S2))
+
+        R = np.vstack(( np.kron(np.eye(N), G - T), np.kron(np.eye(N), -G - T)))
         F = np.kron(np.eye(N), T)
 
+
         # Linear constraints
-        qp_A = np.vstack((R - S.dot(B_tilde), F))
+        qp_A = spa.csc_matrix(np.vstack((R - S.dot(B_tilde), F)))
 
         # upper bound
-        qp_u = np.append(np.zeros(3 * N), np.ones(3 * N))
+        qp_u = np.append(np.zeros(6 * N), np.ones(3 * N))
 
         # lower bound
         qp_l = - qp_u
+
+
+        # Constrain bounds to be within -1 and 1 TODO: Maybe not necessary!
+        # u_sw_idx = np.append(np.ones(3), np.zeros(3))
+        # u_sw_idx = np.tile(u_sw_idx, N)
+        # u_sw_idx = np.flatnonzero(u_sw_idx)
+        # qp_A, qp_l, qp_u = self.add_bounds(u_sw_idx, -1., 1.,
+        #                                    qp_A, qp_l, qp_u)
+
 
         # SA_tilde needed to update bounds
         qp_SA_tilde = S.dot(A_tilde)
@@ -115,10 +128,10 @@ class MIQP(object):
         '''
 
         # Inequality matrix
-        self.P = spa.csc_matrix(qp_P)
-        self.A = spa.csc_matrix(qp_A)
-        self.q_x = qp_q_x
-        self.q_u = qp_q_u
+        self.P = qp_P
+        self.A = qp_A
+        self.q_x = np.asarray(qp_q_x)
+        self.q_u = np.asarray(qp_q_u).flatten()
         self.u = qp_u
         self.l = qp_l
         self.SA_tilde = qp_SA_tilde
@@ -127,3 +140,32 @@ class MIQP(object):
         self.const_r = qp_const_r
         self.N = N
         self.i_idx = i_idx
+
+
+    def add_bounds(self, i_idx, l_new, u_new, A, l, u):
+        """
+        Add new bounds on the variables
+
+            l_new <= x_i <= u_new for i in i_idx
+
+        It is done by adding rows to the contraints
+
+            l <= A x <= u
+        """
+
+        n = A.shape[1]
+
+        # Enforce integer variables to be binary => {0, 1}
+        I_int = spa.identity(n).tocsc()
+        I_int = I_int[i_idx, :]
+        l_int = np.empty((n,))
+        l_int.fill(l_new)
+        l_int = l_int[i_idx]
+        u_int = np.empty((n,))
+        u_int.fill(u_new)
+        u_int = u_int[i_idx]
+        A = spa.vstack([A, I_int]).tocsc()      # Extend problem constraints matrix A
+        l = np.append(l, l_int)         # Extend problem constraints
+        u = np.append(u, u_int)         # Extend problem constraints
+
+        return A, l, u