aefa6b3b71abf97f7aecf64f28848240cfb85a5a
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-03-10 16:24:30 +0000

    Added random qp examples with both solvers

--- examples/random_qp/run_example.py
+++ examples/random_qp/run_example.py
@@ -8,8 +8,15 @@ import scipy as sp
 import scipy.sparse as spa
 import numpy as np
 import numpy.linalg as la
+import pandas as pd
+
+
 import mathprogbasepy as mpbpy
 import miosqp
+
+
+ # Import progress bar
+from tqdm import tqdm
 
 # Reload miosqp module
 try:
@@ -19,12 +26,113 @@ except NameError:
 
 reload(miosqp)
 
+
+def solve(n_vec, m_vec, p_vec, repeat, dns_level, seed, solver='gurobi'):
+    """
+    Solve random optimization problems
+    """
+
+    print("Solving random problems with solver %s\n" % solver)
+
+    # Define statistics to record
+    avg_solve_time = np.zeros(len(n_vec))
+    min_solve_time = np.zeros(len(n_vec))
+    max_solve_time = np.zeros(len(n_vec))
+
+    # reset random seed
+    np.random.seed(seed)
+
+    for i in range(n_prob):
+
+        # Get dimensions
+        n = n_vec[i]
+        m = m_vec[i]
+        p = p_vec[i]
+
+        print("problem n = %i, m = %i, p = %i" % (n, m, p))
+
+        # Define vector of cpu times
+        solve_time_temp = np.zeros(repeat)
+
+        for j in tqdm(range(repeat)):
+
+            # Generate random vector of indeces
+            i_idx = np.random.choice(np.arange(0, n), p, replace=False)
+
+            # Generate random Matrices
+            Pt = spa.random(n, n, density=dns_level)
+            P = spa.csc_matrix(np.dot(Pt.T, Pt))
+            q = sp.randn(n)
+            A = spa.random(m, n, density=dns_level)
+            u = 3 + sp.randn(m)
+            l = -3 + sp.randn(m)
+
+            # Enforce [0, 1] bounds on variables
+            A, l, u = miosqp.add_bounds(i_idx, 0., 1., A, l, u)
+
+            if solver == 'gurobi':
+                # Solve with gurobi
+                prob = mpbpy.QuadprogProblem(P, q, A, l, u, i_idx)
+                res_gurobi = prob.solve(solver=mpbpy.GUROBI, verbose=False)
+                solve_time_temp[j] = res_gurobi.cputime
+
+            elif solver == 'miosqp':
+                # Define problem settings
+                miosqp_settings = {'eps_int_feas': 1e-03,   # integer feasibility tolerance
+                                   'max_iter_bb': 1000,     # maximum number of iterations
+                                   'tree_explor_rule': 1,   # tree exploration rule
+                                                            #   [0] depth first
+                                                            #   [1] two-phase: depth first  until first incumbent and then  best bound
+                                   'branching_rule': 0,     # branching rule
+                                                            #   [0] max fractional part
+                                   'verbose': False,
+                                   'print_interval': 1}
+
+                osqp_settings = {'eps_abs': 1e-04,
+                                 'eps_rel': 1e-04,
+                                 'eps_inf': 1e-04,
+                                 'rho': 0.005,
+                                 'sigma': 0.01,
+                                 'alpha': 1.5,
+                                 'polish': False,
+                                 'max_iter': 2000,
+                                 'verbose': False}
+
+                work = miosqp.miosqp_solve(P, q, A, l, u, i_idx,
+                                           miosqp_settings, osqp_settings)
+                if work.status != miosqp.MI_SOLVED:
+                    import ipdb; ipdb.set_trace()
+                solve_time_temp[j] = work.run_time
+
+
+
+        # Get time statistics
+        avg_solve_time[i] = np.mean(solve_time_temp)
+        max_solve_time[i] = np.max(solve_time_temp)
+        min_solve_time[i] = np.min(solve_time_temp)
+
+    # Create pandas dataframe for the results
+    timings = pd.DataFrame({ 'n' : n_vec,
+                                    'm' : m_vec,
+                                    'p' : p_vec,
+                                    't_min' : min_solve_time,
+                                    't_max' : max_solve_time,
+                                    't_avg' : avg_solve_time })
+
+    return timings
+
+
+
+
 if __name__ == "__main__":
 
     # General settings
-    n_prob = 8        # Number of problems in problem set
-    problem_set = 1   # Problem sets 1 (q << n) or 2 (q = n)
-    cond_P = 10**6     # Condition number of cost function
+    n_prob = 8                  # Number of problems in problem set
+    n_repeat = 10               # Number of repetitions for each problem
+    problem_set = 1             # Problem sets 1 (q << n) or 2 (q = n)
+    density_level = 0.6         # density level for sparse matrices
+    random_seed = 0              # set random seed to make results reproducible
+
 
     if problem_set == 1:
         n_arr = np.array([10, 10,  50, 50,  100, 100, 150, 150])
@@ -34,133 +142,18 @@ if __name__ == "__main__":
     # Other problems n = q
     elif problem_set == 2:
         n_arr = np.array([2, 4, 8, 12, 20, 25, 30])
-        m_arr = 5 * n
-        q_arr = n
+        m_arr = 5 * n_arr
+        q_arr = n_arr
+
+    timings_gurobi = solve(n_arr, m_arr, p_arr, n_repeat,
+                           density_level, random_seed, solver='gurobi')
+
+    timings_miosqp = solve(n_arr, m_arr, p_arr, n_repeat,
+                           density_level, random_seed, solver='miosqp')
 
 
+    print("\nResults GUROBI")
+    print(timings_gurobi)
 
-
-    for i in range(1):
-
-        # Get dimensions
-        n = 3
-        # n = n_arr[i]
-        m = m_arr[i]
-        p = p_arr[i]
-
-
-        # Generate random program
-        log_cond_P = np.log(cond_P)
-        exp_vec = np.arange(-log_cond_P/4., log_cond_P * (n + 1)/(4 * (n - 1)), log_cond_P/(2.*(n-1)))
-        s = np.exp(exp_vec)
-        S = np.diag(s)
-        U, _ = la.qr((np.random.rand(n, n) - 5.) * 200)
-        V, _ = la.qr((np.random.rand(n, n) - 5.) * 200)
-        P = U.dot(S).dot(V.T)
-        P = P.dot(P.T)
-
-        import ipdb; ipdb.set_trace()
-
-
-    # # Nasty problem
-    # n = 100
-    # m = 500
-    # np.random.seed(7)
-    #
-    # # Random Example
-    # # n = 50
-    # # m = 80
-    # # np.random.seed(7)
-    #
-    # # Other example
-    # # n = 20
-    # # m = 30
-    # # np.random.seed(7)
-    #
-    #
-    #
-    # # 10 times slower than gurobi
-    # # i_idx = np.random.choice(np.arange(1,n+1), (int(n/2)), replace=False)
-    # i_idx = np.random.choice(np.arange(0,n), (int(n/3)), replace=False)
-    #
-    #
-    # # Generate random Matrices
-    # Pt = sp.randn(n, n)
-    # P = spa.csc_matrix(np.dot(Pt.T, Pt))
-    # q = sp.randn(n)
-    # A = spa.csc_matrix(sp.randn(m, n))
-    # u = 3 + sp.randn(m)
-    # # l = u
-    # l = -3 + sp.randn(m)
-    # # i_idx = np.array([0, 2, 3, 7])  # index of integer variables
-    #
-    #
-    #
-    # # Enforce integer variables to be binary => {0, 1}
-    # # I_int = spa.identity(n).tocsc()
-    # # I_int = I_int[i_idx, :]
-    # # l_int = np.empty((n,))
-    # # l_int.fill(0.)
-    # # l_int = l_int[i_idx]
-    # # u_int = np.empty((n,))
-    # # u_int.fill(1)
-    # # u_int = u_int[i_idx]
-    # # A = spa.vstack([A, I_int]).tocsc()      # Extend problem constraints matrix A
-    # # l = np.append(l, l_int)         # Extend problem constraints
-    # # u = np.append(u, u_int)         # Extend problem constraints
-    # A, l, u = miosqp.add_bounds(i_idx, 0., 1., A, l, u)
-    #
-    #
-    #
-    #
-    #
-    #
-    # # Create MIQP problem
-    # prob = mpbpy.QuadprogProblem(P, q, A, l, u, i_idx)
-    # resGUROBI = prob.solve(solver=mpbpy.GUROBI)
-    # # resCPLEX = prob.solve(solver=mpbpy.CPLEX)
-    #
-    #
-    # # Try miOSQP
-    #
-    # # Define problem settings
-    # miosqp_settings = {'eps_int_feas': 1e-03,   # integer feasibility tolerance
-    #                    'max_iter_bb': 10000,     # maximum number of iterations
-    #                    'tree_explor_rule': 1,   # tree exploration rule
-    #                                             #   [0] depth first
-    #                                             #   [1] two-phase: depth first  until first incumbent and then  best bound
-    #                    'branching_rule': 0,     # branching rule
-    #                                             #   [0] max fractional part
-    #                    'verbose': True,
-    #                    'print_interval': 1}
-    #
-    # osqp_settings = {'eps_abs': 1e-04,
-    #                  'eps_rel': 1e-04,
-    #                  'eps_inf': 1e-04,
-    #                  'rho': 0.005,
-    #                  'sigma': 0.01,
-    #                  'alpha': 1.5,
-    #                  'polish': False,
-    #                  'max_iter': 2000,
-    #                  'verbose': False}
-    #
-    # work = miosqp.miosqp_solve(P, q, A, l, u, i_idx,
-    #                            miosqp_settings, osqp_settings,
-    #                         #    resGUROBI.x
-    #                            )
-    #
-    #
-    #
-    # print("\n\n\nDifference solutions miOSQP and GUROBI               %.4e" % la.norm(resGUROBI.x - work.x))
-    # # print("\n\n\nDifference solutions miOSQP and CPLEX               %.4e" % la.norm(resCPLEX.x - work.x))
-    #
-    #
-    # print("miOSQP")
-    # print("------")
-    # print("Objective value       %.4e" % work.upper_glob)
-    # print("Elapsed time          %.4e\n" % work.run_time)
-    #
-    # print("GUROBI")
-    # print("------")
-    # print("Objective value       %.4e" % resGUROBI.obj_val)
-    # print("Elapsed time          %.4e" % resGUROBI.cputime)
+    print("\nResults MIOSQP")
+    print(timings_miosqp)
